a4d6226d74aa6e95adba29e879be88e0988b3efb
Yuri F. Marques <yuri.marques.dev@gmail.com>
2022-12-28 10:24:50 +0100

    chore: added a Dockerfile and docker-compose to the project

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,7 @@ import Config
 
 # Configure your database
 config :shortener, Shortener.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "shortener_dev",
+  url: System.get_env("DATABASE_URL"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +16,7 @@ config :shortener, Shortener.Repo,
 config :shortener, ShortenerWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
